313212842badc41e7b0becc09468a4e76c9d9a72
mikkeboss <mikkeboss@users.noreply.github.com>
2016-05-29 01:56:03 +0200

    cleaning code

--- stazkainos/Controllers/HomeController.cs
+++ stazkainos/Controllers/HomeController.cs
@@ -16,30 +16,9 @@ namespace stazkainos.Controllers
             //List<FundValue> fundList = new List<FundValue>();
             //fundList = reader.Parse();
             // DatabaseContext context = new DatabaseContext();
-            // var a =context.Database.ExecuteSqlCommand("GetFunds");
-            // var f = context.Funds;
-            // context.Funds.AddRange(fundList);
-            // context.SaveChanges();
-            // var ff = context.Funds;
-            DatabaseHandler db = new DatabaseHandler();
-            var temp = db.GetFundValues();
-
-
-
-
-
-
-
-
-
-            // context.Dispose();
             return View();
         }
 
-        public ActionResult dd()
-        {
-            return View();
-
-        }
+        
     }
 }
